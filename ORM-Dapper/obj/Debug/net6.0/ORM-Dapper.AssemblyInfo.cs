9e5195baf68bafe0f5c276eb148fdb1d2d0a49b5
Corey Schroeder <codingcorey365@gmail.com>
2024-04-28 15:40:55 -0500

    reverting

--- ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
+++ ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1399071f6d5a1c780ab374e55a0f5e4a27f80581")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305a00a6010c774293b1962694b7eebe0da4b46c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
